f009cafcff0ce9a7e0f84ffb4dc9a7480bc80015
David Gonen <david@curiousminds.com>
2015-08-03 12:15:30 -0700

    add new item working.  need to fix remove item functionality

--- js/script.js
+++ js/script.js
@@ -5,19 +5,28 @@ $(document).ready(function(){
     increaseArea: '50%' // optional
   });
 
-//cross out the text of items with the checkbox selected
+  //cross out the text of items with the checkbox selected
   $('div.red-line ins.iCheck-helper').click(function() {
       $('.icheckbox_square-red').parent().next().css("text-decoration", "none");
       $('.icheckbox_square-red.checked').parent().next().css("text-decoration", "line-through");
   });
 
-//remove lines
+  //remove lines
 
   $('.lined-box button').click(function() {
     $( this ).parent().slideUp();
 
     });
 
+  //add items
+
+  $('.lined-box-first > div > button').click(function() {
+      console.log("add button clicked!");
+      var itemvalue = $('.lined-box-first .todo-item > input').val();
+      console.log(itemvalue);
+      $( 'body .line-item:last-of-type' ).clone().insertAfter('body .line-item:last-of-type');
+      $( 'body .line-item:last-of-type .todo-item > span' ).text( itemvalue );   
+  });
 
 
 
