1b108990f762375a70030acfa5794e83c94c9ee2
David Gonen <david@curiousminds.com>
2015-08-02 18:10:42 -0700

    going to try to use pure grid

--- js/script.js
+++ js/script.js
@@ -2,6 +2,6 @@ $(document).ready(function(){
   $('input').iCheck({
     checkboxClass: 'icheckbox_square-red',
     radioClass: 'iradio_square-red',
-    increaseArea: '70%' // optional
+    increaseArea: '100%' // optional
   });
 });
