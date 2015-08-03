ea642d1c818e917164b3101a93187fc2781f2eaa
David Gonen <david@curiousminds.com>
2015-08-03 10:44:01 -0700

    got the remove button working

--- js/script.js
+++ js/script.js
@@ -5,10 +5,20 @@ $(document).ready(function(){
     increaseArea: '50%' // optional
   });
 
+//cross out the text of items with the checkbox selected
   $('div.red-line ins.iCheck-helper').click(function() {
       $('.icheckbox_square-red').parent().next().css("text-decoration", "none");
       $('.icheckbox_square-red.checked').parent().next().css("text-decoration", "line-through");
   });
+
+//remove lines
+
+  $('.lined-box button').click(function() {
+    $( this ).parent().slideUp();
+
+    });
+
+
 
 
 });
