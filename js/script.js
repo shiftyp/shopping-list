0bbfde42ff7df77a8934b165c0a4a2974ffebabf
David Gonen <david@curiousminds.com>
2015-08-22 16:29:11 +0300

    trying to get add todo item working

--- js/script.js
+++ js/script.js
@@ -1,68 +1,45 @@
 $(document).ready(function(){
 
-  $('body').on('click','', function() {
-    console.log( event.target );
-  });
-
   //cross out the text of items with the checkbox selected
-  $('body').on('click', 'ins.iCheck-helper', function() {
-      $('.icheckbox_square-red').parent().next().children('span').css("text-decoration", "none");
-      $('.icheckbox_square-red.checked').parent().next().children('span').css("text-decoration", "line-through");
+  $('ul').on('click', 'input', function() {
+    $( this ).closest('li').find('div.lined-box div.todo-item span').toggleClass('has-line');
   });
 
 
-  $('body').on('ifToggled', function(event){
-    console.log(event.type + ' callback');
-  });
-
-    //add items
-    $('body').on('click', '.button-success', function() {
-      var itemvalue = $('.lined-box-first .todo-item > input').val();
-      console.log( event.target );
-      $('body ul li:last-of-type').clone().appendTo('body ul');
-      $('body ul li:last-of-type .todo-item span').text( itemvalue );
-      $('body .li:last-of-type input').remove();
-      $('body .li:last-of-type input').iCheck({
-        checkboxClass: 'icheckbox_square-red',
-        radioClass: 'iradio_square-red',
-        increaseArea: '50%', // optional
-        inheritID: true
-      });
-      $('body ul').sortable();
-    });
-
-
-    // $('body').on('click', '.lined-box-first > div > button', function() {
-    //     console.log("add button clicked!");
-    //     var itemvalue = $('.lined-box-first .todo-item > input').val();
-    //     console.log(itemvalue);
-    //     $( 'body .line-item:last-of-type' ).clone().insertAfter('body .line-item:last-of-type');
-    //     $( 'body .line-item:last-of-type .todo-item > span' ).text( itemvalue );
-    //     $('body .line-item:last-of-type input').remove();
-    //     $('body .line-item:last-of-type input').iCheck({
-    //       checkboxClass: 'icheckbox_square-red',
-    //       radioClass: 'iradio_square-red',
-    //       increaseArea: '50%', // optional
-    //       inheritID: true
-    //     });
-    //     $('body div.line-item').sortable();
-    // });
-
-  //remove lines
+  //REMOVE TO DO ITEM
   $('body').on('click', 'ul li.line-item .lined-box .button-error', function( event ) {
     console.log( event.target );
-    $( event.target ).closest('li.line-item').slideUp();
-
+    $( event.target ).closest('li.line-item').slideUp(function () {
+      $( this ).remove();
 
     });
 
 
-    $('input').iCheck({
-      checkboxClass: 'icheckbox_square-red',
-      radioClass: 'iradio_square-red',
-      increaseArea: '50%', // optional
-      inheritID: true
     });
+
+   //input validation
+   $('body').on('click', '.button-success', function() {
+     var inputVal = $('.lined-box-first .todo-item > input').val();
+     if (inputVal == "") {
+      $('div.validation').text("please enter some text!");
+     }
+     else {
+       $('div.validation').css('display', 'none');
+       //ADD TO DO ITEMS
+       $('body').on('click', '.button-success', function() {
+         var itemValue = $('.lined-box-first .todo-item > input').val();
+         $('li.template')
+           .clone()
+           .appendTo('body ul')
+           .find('span').text( itemValue);
+          $('ul > li.line-item:last-of-type').removeClass('template');
+       $('body ul').sortable();
+       });
+     }
+
+
+   });
+
 
 $('body div.line-item').sortable();
 
