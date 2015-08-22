cb2ed513fd423ec78c27748a020e51602deea230
Ryan Lynch <ryan.lynch@it.com>
2015-08-22 12:07:36 -0400

    Made some views! An input view to handle taking new list item data from the user, and a list view to handle displaying and interacting with the list.

--- js/script.js
+++ js/script.js
@@ -1,46 +1,60 @@
 $(document).ready(function(){
+	 //input validation
+	
+	function InputView() {
+		this.element = $('.add-box');
+		this.button = this.element.find('button');
+		this.input = this.element.find('input');
+		this.validationMessage = this.element.find('.validation');
+		
+		this.button.click(this.validateInput.bind(this));
+	}
 
-  //cross out the text of items with the checkbox selected
-  $('ul').on('click', 'input', function() {
-    $( this ).closest('li').find('div.lined-box div.todo-item span').toggleClass('has-line');
-  });
+	
+	InputView.prototype.validateInput = function() {
+		var inputVal = $('.lined-box-first .todo-item > input').val();
 
+		if (inputVal == "") {
+			this.validationMessage.text("please enter some text!");
+		} else {
+			this.validationMessage.css('display', 'none');
+		}
 
-  //REMOVE TO DO ITEM
-  $('body').on('click', 'ul li.line-item .lined-box .button-error', function( event ) {
-    console.log( event.target );
-    $( event.target ).closest('li.line-item').slideUp(function () {
-      $( this ).remove();
+		listView.addListItem(inputVal);
+	}
 
-    });
+	var inputView = new InputView();
 
+	//----------------------------------
+	
+	function ListView() {
+		this.element = $('.shopping-list');
 
-    });
+		this.element.on('click', 'input', this.checkListItem.bind(this));
+		this.element.on('click', '.button-error', this.removeListItem.bind(this));
+	}
 
-   //input validation
-   $('body').on('click', '.button-success', function() {
-     var inputVal = $('.lined-box-first .todo-item > input').val();
-     if (inputVal == "") {
-      $('div.validation').text("please enter some text!");
-     }
-     else {
-       $('div.validation').css('display', 'none');
-       //ADD TO DO ITEMS
-       $('body').on('click', '.button-success', function() {
-         var itemValue = $('.lined-box-first .todo-item > input').val();
-         $('li.template')
-           .clone()
-           .appendTo('body ul')
-           .find('span').text( itemValue);
-          $('ul > li.line-item:last-of-type').removeClass('template');
-       $('body ul').sortable();
-       });
-     }
+	ListView.prototype.addListItem = function(value) {
+		$('li.template')
+			.clone()
+			.appendTo('body ul')
+			.removeClass('template')
+			.find('span').text( value);
+	};
 
+	//cross out the text of items with the checkbox selected
+	
+	ListView.prototype.checkListItem = function(event) {
+		$( event.target ).closest('li').find('div.lined-box div.todo-item span').toggleClass('has-line');
+	};
 
-   });
+	//REMOVE TO DO ITEM
+	
+	ListView.prototype.removeListItem = function(event) {
+		$( event.target ).closest('li.line-item').slideUp(function () {
+			$( this ).remove();
+		});
+	};
 
-
-$('body div.line-item').sortable();
-
+	var listView = new ListView();
 });
